ad0d297c42b5bb7d7166abf6bd66b35c19751424
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 23:24:40 +0300

    feat: add API component to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Services from './components/Services';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Jokes from './components/Jokes';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
     <Projects />
     <Contact />
     <Footer />
+    <Jokes />
     </>
     
   );
