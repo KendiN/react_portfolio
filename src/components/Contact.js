3edb81b0a4f38af5246baff3cba063a3b6097d22
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 22:20:35 +0300

    feat: add Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Contact() {
+  return (
+<section className="contact" id="contact">
+    <h2 className="heading">Contact <span>Me</span></h2>
+
+    <form action="#">
+        <div className="input-box">
+            <input type="text" placeholder="Full Name" />
+            <input type="email" placeholder="Email Address" />
+        </div>
+
+        <div className="input-box">
+            <input type="number" placeholder="Mobile Number" />
+            <input type="text" placeholder="Email Subject" />
+        </div>
+        
+        <textarea name="" id="" cols="30" rows="10" placeholder="Writa a message"></textarea>
+        <input type="submit" value="Send Message" className="btn" />
+    </form>
+</section>
+  )
+}
+
+export default Contact
