a9bbdc5d4db8d5e5638e565834321e46308c448b
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 22:15:10 +0300

    feat: add Services component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
+import Services from './components/Services';
 import Footer from './components/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Header />
     <Home/>
     <About />
+    <Services />
     <Footer />
     </>
     
---
+++ src/components/Services.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+function Services() {
+  return (
+    <section className="services" id="services">
+    <h2 className="heading">My <span>Services</span></h2>
+    <h3>What I DO</h3>
+
+    <div className="services-container">
+        <div className="services-box">
+            <i className='bx bx-code-block'></i>
+            <h3>Web Development</h3>
+            <p>
+                I build eye-catching websites that are both functional and 
+                visually appealing,using a mix of HTML, CSS, and JavaScript 
+                to create a seamless online experience.
+            </p>
+        </div>
+
+        <div className="services-box">
+            <i className='bx bx-code-alt' ></i>
+            <h3>UI/UX Design</h3>
+            <p>
+                Crafting user-friendly interfaces and smooth experiences, 
+                my design services focus on making websites intuitive and 
+                visually engaging, ensuring a delightful journey for users.
+
+            </p>
+        </div>
+
+        <div className="services-box">
+            <i className='bx bx-mobile' ></i>
+            <h3>Mobile Development</h3>
+            <p>
+                Transforming ideas into user-friendly mobile apps, 
+                I use technologies like React Native to create 
+                innovative and responsive applications, bringing 
+                concepts to life in the mobile space.
+            </p>
+        </div>
+    </div>
+
+</section>
+  )
+}
+
+export default Services
