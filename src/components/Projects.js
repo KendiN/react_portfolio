33990c81be3982ddf3ce00059a39691e1ed5effa
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 23:01:33 +0300

    fix; resolve icons issue in projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { BiLinkExternal } from "react-icons/bi";
 
 
 function Projects() {
@@ -16,7 +16,7 @@ function Projects() {
                     Developed a website to be used in the event of an Emergency
                     to assist users to locate the nearest Emergency service providers
                     closest to them.
-                    <a href="https://github.com/KendiN/Emergency-Aid-System"><i className="bx bx-link-external"></i></a>
+                    <a href="https://github.com/KendiN/Emergency-Aid-System"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
@@ -29,7 +29,7 @@ function Projects() {
                     to recommend online courses to students 
                     based on their career interests around
                     academic performance.
-                    <a href="#"><i className="bx bx-link-external"></i></a>
+                    <a href="#"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
@@ -41,7 +41,7 @@ function Projects() {
                 <p>Develeloped a website designed to help in the 
                     management of a Restuarant. Tracks all the 
                     orders and payments made by customers.
-                    <a href="#"><i className="bx bx-link-external"></i></a>
+                    <a href="#"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
@@ -52,7 +52,7 @@ function Projects() {
                 <h4>Elearning esoma website</h4>
                 <p>Redesigned the esoma elearning website and improving
                     the user design and experience of the wesbite.
-                    <a href="https://github.com/KendiN/esoma"><i className="bx bx-link-external"></i></a>
+                    <a href="https://github.com/KendiN/esoma"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
@@ -64,7 +64,7 @@ function Projects() {
                 <p>Developed a mobile applications 
                     that allows users to track the books they have
                     read and  their give reviews.
-                    <a href="#"><i className="bx bx-link-external"></i></a>
+                    <a href="#"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
@@ -76,7 +76,7 @@ function Projects() {
                 <p>Redesigned the Dashboard for co-operative banks
                     logs by trying to and improve
                     the user design and experience.
-                    <a href="#"><i className="bx bx-link-external"></i></a>
+                    <a href="#"><i className="bx bx-link-external"></i><BiLinkExternal /></a>
                 </p>
             </div>
         </div>
