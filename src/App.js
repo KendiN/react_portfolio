a5a871a762ced8eeb7da9003f444031f9fc468d7
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 22:19:02 +0300

    feat: add Projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
+import Projects from './components/Projects';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Home/>
     <About />
     <Services />
+    <Projects />
     <Footer />
     </>
     
---
+++ src/components/Projects.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+
+
+function Projects() {
+  return (
+    <section className="projects" id="projects">
+    <h2 className="heading">Latest <span>Projects</span></h2> 
+
+    <div className="projects-container">
+        <div className="projects-box">
+            <img src="assets/images/ambulance.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Emergency Aid Website</h4>
+                <p>
+                    Developed a website to be used in the event of an Emergency
+                    to assist users to locate the nearest Emergency service providers
+                    closest to them.
+                    <a href="https://github.com/KendiN/Emergency-Aid-System"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <div className="projects-box">
+            <img src="assets/images/course1.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Online Courses Recommendations</h4>
+                <p>Developed a website using python and Django
+                    to recommend online courses to students 
+                    based on their career interests around
+                    academic performance.
+                    <a href="#"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <div className="projects-box">
+            <img src="assets/images/rest.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Restuarant Management Website</h4>
+                <p>Develeloped a website designed to help in the 
+                    management of a Restuarant. Tracks all the 
+                    orders and payments made by customers.
+                    <a href="#"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <div className="projects-box">
+            <img src="assets/images/school.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Elearning esoma website</h4>
+                <p>Redesigned the esoma elearning website and improving
+                    the user design and experience of the wesbite.
+                    <a href="https://github.com/KendiN/esoma"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <div className="projects-box">
+            <img src="assets/images/books.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Book review mobile app</h4>
+                <p>Developed a mobile applications 
+                    that allows users to track the books they have
+                    read and  their give reviews.
+                    <a href="#"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <div className="projects-box">
+            <img src="assets/images/dash.jpg" alt="" />
+            <div className="projects-layer">
+                <h4>Bank Logs Dashboard</h4>
+                <p>Redesigned the Dashboard for co-operative banks
+                    logs by trying to and improve
+                    the user design and experience.
+                    <a href="#"><i className="bx bx-link-external"></i></a>
+                </p>
+            </div>
+        </div>
+
+    </div>
+</section>
+  )
+}
+
+export default Projects
