03e00ea51a93cb33ca5b81687cd4d5fd3b08b5dd
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-01-29 22:05:53 +0300

    feat: add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+    <div className="footer-text">
+        <p>Copyright &copy; 2023 by Kendi Njeru | All Rights Reserved.</p>
+    </div>
+
+    <div className="footer-iconTop">
+        <a href="#home" className="bx bx-up-arrow-alt"></a>
+    </div>
+</footer>
+  )
+}
+
+export default Footer
